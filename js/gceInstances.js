90820140dc55ea81e92fcb5d5c70f4e8af5df4cc
danielrivkin <daniel@doit-intl.com>
2015-06-04 18:50:32 +0300

    Flickering issues fixed, etc.

--- js/gceInstances.js
+++ js/gceInstances.js
@@ -6,6 +6,8 @@ var app = angular.module('app', ['ngMaterial']);
 
 app.controller('Controller', ['$scope', '$http', '$window', '$location', '$filter', function ($scope, $http, $window, $location, $filter) {
 
+    var filteredDataTemp;
+
     $scope.head = {
         "instance_type": "Name",
         "family": "Family",
@@ -20,10 +22,10 @@ app.controller('Controller', ['$scope', '$http', '$window', '$location', '$filte
     };
 
     $scope.costs = [ {name:'hourly', 'mult': 1},
-                     {name:'daily', 'mult': 24},
-                     {name:'weekly', 'mult': 168},
-                     {name:'monthly', 'mult': 732},
-                     {name:'anually', 'mult': 8760}
+        {name:'daily', 'mult': 24},
+        {name:'weekly', 'mult': 168},
+        {name:'monthly', 'mult': 732},
+        {name:'anually', 'mult': 8760}
     ];
 
     $scope.regions = [];
@@ -34,10 +36,10 @@ app.controller('Controller', ['$scope', '$http', '$window', '$location', '$filte
 
     $scope.sort = {
         column: 'instance_type',
-        descending: false
+        descending: true
     };
 
-    $scope.showArrow = [];
+    $scope.sortOrder = [];
 
     $scope.getAllRegionsNFamilies = function() {
 
@@ -52,8 +54,9 @@ app.controller('Controller', ['$scope', '$http', '$window', '$location', '$filte
 
     $scope.filterData = function() {
 
-        $scope.filteredData = [];
+        //$scope.filteredData = [];
         var allDataTemp = angular.copy($scope.allData);
+        filteredDataTemp = [];
 
         angular.forEach(allDataTemp, function(instance, i) {
             if (instance.family == $scope.familyFilter || $scope.familyFilter == $scope.families[0]) {
@@ -75,10 +78,14 @@ app.controller('Controller', ['$scope', '$http', '$window', '$location', '$filte
                 });
                 //instance.currPricing = filteredPricing;
                 instance.pricing = {};
-                $scope.filteredData = $scope.filteredData.concat(instance);
-                $scope.orderByFunc($scope.sort.column);
+                filteredDataTemp = filteredDataTemp.concat(instance);
             }
         });
+        //$scope.filteredData = [];
+        $scope.filteredData = filteredDataTemp;
+
+        $scope.orderByFunc($scope.sort.column);
+
     }
 
     $scope.selectedCls = function(column) {
@@ -93,25 +100,25 @@ app.controller('Controller', ['$scope', '$http', '$window', '$location', '$filte
             sort.column = column;
             sort.descending = false;
         }
-        if (sort.descending === false) { $scope.sortOrder = 'fa-angle-double-up'; }
-        else { $scope.sortOrder = 'fa-angle-double-down'; }
+        if (sort.descending === false) { $scope.sortOrder[column] = 'fa-angle-double-up'; }
+        else { $scope.sortOrder[column] = 'fa-angle-double-down'; }
     };
 
     $scope.orderByFunc = function(column) {
-        //if (column.indexOf('cost') !== -1) {
 
-        //} else {
-            $scope.filteredData = $filter('orderBy')($scope.filteredData, column, $scope.sort.descending);
-        //}
+        $scope.changeSorting(column);
+
+        $scope.filteredData = $filter('orderBy')($scope.filteredData, column, $scope.sort.descending);
 
         angular.forEach($scope.head, function(name, column) {
 
             if (column === $scope.sort.column) {
-                $scope.showArrow[column] = true;
+                $scope.sortOrder[column] += ' activeColumn';
             } else {
-                $scope.showArrow[column] = false;
+                $scope.sortOrder[column] += ' passiveColumn';
             }
         });
+        console.log($scope.sortOrder);
     }
 
     $http.get('scraper/instances.json').success (function(data){
@@ -125,7 +132,16 @@ app.controller('Controller', ['$scope', '$http', '$window', '$location', '$filte
         $scope.costFilter = $scope.costs[0].name;
 
         $scope.filterData();
-        $scope.sortOrder = 'fa-angle-double-up'
+        angular.forEach($scope.head, function(name, column) {
+
+            $scope.sortOrder[column] = 'fa-angle-double-up';
+            if (column === 'instance_type') {
+                $scope.sortOrder[column] += ' activeColumn';
+            } else {
+                $scope.sortOrder[column] += ' passiveColumn';
+            }
+        });
+
     });
 
 }]);
